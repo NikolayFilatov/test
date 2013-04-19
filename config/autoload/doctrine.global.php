bc224b76439a9795704562b76db7b1ed6d075d4f
Nikolayfilatov <2519483@gmail.com>
2013-04-19 20:32:22 +0400

    +

--- config/autoload/doctrine.global.php
+++ config/autoload/doctrine.global.php
@@ -8,7 +8,7 @@
                         'host'     => 'localhost',
                         'port'     => '3306',
                         'user'     => 'root',
-                        'password' => '',
+                        'password' => 'pass',
                         'dbname'   => 'bludo',
                         'charset' => 'utf8',
                         'driverOptions' => array(
